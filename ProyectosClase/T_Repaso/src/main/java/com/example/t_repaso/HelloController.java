06a5c6a432ecac4d32403ac2c038441ec96466e4
Joseverazz <verdezoto.vera@gmail.com>
2021-10-26 17:29:39 +0200

    V1

--- ProyectosClase/T_Repaso/src/main/java/com/example/t_repaso/HelloController.java
+++ ProyectosClase/T_Repaso/src/main/java/com/example/t_repaso/HelloController.java
@@ -1,14 +1,44 @@
 package com.example.t_repaso;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    Button botonUno,botonDos,botonTres;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    private void acciones(){
+        botonUno.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Pulsado 1");
+            }
+        });
+
+        botonDos.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Pulsado 2");
+            }
+        });
+
+        botonTres.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Pulsado 3");
+            }
+        });
     }
 }
